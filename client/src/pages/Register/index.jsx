e47b5a303dfe903b16d8f5d251cd0b0692050496
Emmyojile <emmaojile99@gmail.com>
2023-09-12 15:45:30 -0800

    Pushing to github

---
+++ client/src/pages/Register/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='flex h-screen justify-center items-center bg-primary'>
+        
+    </div>
+  )
+}
+
+export default Register
