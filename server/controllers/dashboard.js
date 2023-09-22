b856a3ca52549dc5d5c48456b856ac61a200e3ec
Emmyojile <emmaojile99@gmail.com>
2023-09-22 07:39:16 -0800

    dashboard queries and api

--- server/controllers/dashboard.js
+++ server/controllers/dashboard.js
@@ -5,11 +5,64 @@ import mongoose from "mongoose";
 //get all blood groups totalIn, totalOut, available data from inventory
 export const AllBloodGroupsData = async (req, res) => {
   try {
-    
-    return res.send({ success: true,
-        message: "Blood Groups Data", 
-        data: []
-     });
+    const allBloodGroups = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+    const bloodGroupsData = [];
+    const organization = new mongoose.Types.ObjectId(req.body.userId);
+
+    await Promise.all(
+      allBloodGroups.map(async (bloodGroup) => {
+        const totalIn = await Inventory.aggregate([
+          {
+            $match: {
+              bloodGroup: bloodGroup,
+              inventoryType: "in",
+              organization,
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              total: {
+                $sum: "$quantity",
+              },
+            },
+          },
+        ]);
+
+        const totalOut = await Inventory.aggregate([
+          {
+            $match: {
+              bloodGroup: bloodGroup,
+              inventoryType: "out",
+              organization,
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              total: {
+                $sum: "$quantity",
+              },
+            },
+          },
+        ]);
+
+        const available = (totalIn[0]?.total || 0) - (totalOut[0]?.total || 0);
+
+        bloodGroupsData.push({
+          bloodGroup,
+          totalIn: totalIn[0]?.total || 0,
+          totalOut: totalOut[0]?.total || 0,
+          available,
+        });
+      })
+    );
+
+    return res.send({
+      success: true,
+      message: "Blood Groups Data",
+      data: bloodGroupsData,
+    });
   } catch (error) {
     return res.send({
       success: false,
